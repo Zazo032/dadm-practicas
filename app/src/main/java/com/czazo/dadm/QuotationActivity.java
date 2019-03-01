38fae327f3e2f09f3eefd8aabd3ced0a7eff48b6
Cristian Zazo <cristian.zazo@gmail.com>
2019-03-01 16:28:38 +0100

    Fixes, ejercicio 3

--- app/src/main/java/com/czazo/dadm/QuotationActivity.java
+++ app/src/main/java/com/czazo/dadm/QuotationActivity.java
@@ -9,7 +9,6 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.czazo.dadm.databases.AbstractRepository;
@@ -20,16 +19,34 @@ public class QuotationActivity extends AppCompatActivity {
     private int contadorCitasRecibidas = 0;
     private AbstractRepository abstractRepository;
     private Menu menu;
+    private boolean isAddVisible = false;
+    final String KEY_QUOTE = "QUOTE", KEY_AUTHOR = "AUTHOR", KEY_COUNT = "COUNT", KEY_ADD = "ADD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
-        final TextView hint = findViewById(R.id.quotation_hint);
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        String name = pref.getString("username", "Nameless One");
-        hint.setText(String.format(getString(R.string.quotation_hint), name));
+        if (savedInstanceState == null) {
+            final TextView hint = findViewById(R.id.quotation_hint);
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+            String name = pref.getString("username", "Nameless One");
+            hint.setText(String.format(getString(R.string.quotation_hint), name));
+        } else {
+            ((TextView) findViewById(R.id.quotation_hint)).setText(savedInstanceState.getString(KEY_QUOTE));
+            ((TextView) findViewById(R.id.sample_author)).setText(savedInstanceState.getString(KEY_AUTHOR));
+            contadorCitasRecibidas = savedInstanceState.getInt(KEY_COUNT);
+            isAddVisible = savedInstanceState.getBoolean(KEY_ADD);
+        }
         abstractRepository = AbstractRepository.getInstance(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_QUOTE, ((TextView) findViewById(R.id.quotation_hint)).getText().toString());
+        outState.putString(KEY_AUTHOR, ((TextView) findViewById(R.id.sample_author)).getText().toString());
+        outState.putInt(KEY_COUNT, contadorCitasRecibidas);
+        outState.putBoolean(KEY_ADD, isAddVisible);
     }
 
     @Override
@@ -37,10 +54,10 @@ public class QuotationActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_quotation, menu);
         this.menu = menu;
+        menu.findItem(R.id.add_quotation).setVisible(isAddVisible);
         return super.onCreateOptionsMenu(menu);
     }
 
-    @SuppressLint("StringFormatInvalid")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -53,15 +70,19 @@ public class QuotationActivity extends AppCompatActivity {
                 String quoteAuthor = anotherAuthor.getText().toString();
 
                 Repository.getInstance(this).addQuote(quoteText, quoteAuthor);
+
+                item.setVisible(false);
+                isAddVisible = false;
                 return true;
             case R.id.refresh_quotation:
                 // TODO: Obtener nueva cita (Práctica 3A)
-                final TextView hint = findViewById(R.id.quotation_hint);
-                contadorCitasRecibidas++;
-                menu.findItem(R.id.add_quotation).setVisible(true);
-                hint.setText(String.format(getString(R.string.quotation_sample_text), contadorCitasRecibidas));
+                TextView hint = findViewById(R.id.quotation_hint);
                 TextView author = findViewById(R.id.sample_author);
+                menu.findItem(R.id.add_quotation).setVisible(true);
+                isAddVisible = true;
+                hint.setText(String.format(getString(R.string.quotation_sample_text), contadorCitasRecibidas));
                 author.setText(String.format(getString(R.string.quotation_sample_author), contadorCitasRecibidas));
+                contadorCitasRecibidas++;
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
